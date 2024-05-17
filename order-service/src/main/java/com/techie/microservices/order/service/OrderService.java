a1bb11fcc9c8197dc1f1ecda867862e8dcaae337
Randy <randytan089@gmail.com>
2024-05-17 15:42:11 -0500

    Created ordering service and tested that it works

---
+++ order-service/src/main/java/com/techie/microservices/order/service/OrderService.java
@@ -0,0 +1,25 @@
+package com.techie.microservices.order.service;
+
+import com.techie.microservices.order.dto.OrderRequest;
+import com.techie.microservices.order.model.Order;
+import com.techie.microservices.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setSkuCode(orderRequest.skuCode());
+        order.setQuantity(orderRequest.quantity());
+        orderRepository.save(order);
+    }
+}
